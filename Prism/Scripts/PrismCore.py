204daba2d89ce5793204f233754c577a48a09a3d
exploreborders <mail@exploreborders.com>
2021-11-18 16:48:18 +0100

    nframes

--- Prism/Scripts/PrismCore.py
+++ Prism/Scripts/PrismCore.py
@@ -183,7 +183,7 @@ class PrismCore:
 
         try:
             # set some general variables
-            self.version = "v1.3.0.84"
+            self.version = "v1.3.0.85"
             self.requiredLibraries = "v1.3.0.0"
             self.core = self
 
