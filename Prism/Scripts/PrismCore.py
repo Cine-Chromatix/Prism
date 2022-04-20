288cfa00ddd18f03a47db07b48ab90a2ebdca971
exploreborders <mail@exploreborders.com>
2022-04-20 16:36:07 +0200

    Update PrismCore.py

--- Prism/Scripts/PrismCore.py
+++ Prism/Scripts/PrismCore.py
@@ -183,7 +183,7 @@ class PrismCore:
 
         try:
             # set some general variables
-            self.version = "v1.3.0.85"
+            self.version = "v1.3.0.86"
             self.requiredLibraries = "v1.3.0.0"
             self.core = self
 
